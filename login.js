c5299f21f21c8eb71f95fe3e6352a3e303000daa
RoshanJose7 <roshanstark007@gmail.com>
2020-09-25 10:33:01 +0530

    Fix: lame errors

--- login.js
+++ login.js
@@ -1,22 +1,20 @@
-function valid(){
-	var button = document.querySelector(".botton");
-	button.addEventListener("click", ()=<{
-		var len, mail, pass;
-		pass =document.getElementById("password").value;
-		mail = document.getElementById("e-mail").value;
-		len = mail.length;
-		if (mail == ""){
-			document.getElementById("email").innerHTML = "Email Required";
-		}
-		else if (mail.slice(len-12, len)!= "@bmsce.ac.in"){
-			document.getElementById("email").innerHTML = "Invalid Email";
-		}
+function valid() {
+  var button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    var len, mail, pass;
+    pass = document.getElementById("password").value;
+    mail = document.getElementById("e-mail").value;
+    len = mail.length;
+    if (mail == "") {
+      document.getElementById("email").innerHTML = "Email Required";
+    } else if (mail.slice(len - 12, len) != "@bmsce.ac.in") {
+      document.getElementById("email").innerHTML = "Invalid Email";
+    }
 
-		if (pass==""){
-			document.getElementById("pwd").innerHTML= "Password Required";
-		}
-	})
-		
-
+    if (pass == "") {
+      document.getElementById("pwd").innerHTML = "Password Required";
+    }
+  });
 }
+
 valid();
